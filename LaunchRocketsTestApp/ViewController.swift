36513dacd09892eb7b2058a14eb11aad3c4919bf
andriibilan <alfabetta09@gmail.com>
2018-08-20 12:08:18 +0300

    Initial Commit

---
+++ LaunchRocketsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchRocketsTestApp
+//
+//  Created by Andrii Bilan on 8/20/18.
+//  Copyright © 2018 Andrii Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
